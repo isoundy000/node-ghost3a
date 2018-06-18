df227de7b240a96b8a52212965e7df5502084b20
yangfanyu <yangfanyu@aliyun.com>
2018-06-18 22:28:29 +0800

    调整细节

--- test/access.js
+++ test/access.js
@@ -1,26 +1,16 @@
-var Access = {
-    auth: function (store, method, req, onSuccess, onError) {
-        console.log('我是自定义的权限验证函数');
-        onSuccess();
-    },
-    increment: {
-        store: 'increment',
-        fields: {
-            test1: 10,
-            test2: 20,
-            test3: 30,
-            test4: 40
-        }
-    },
-    store: {
-        picture: {
-            upload: true,
-            mimeType: {
-                'image/jpeg': 'jpg',
-                'image/png': 'png',
-                'image/gif': 'gif',
-                'image/bmp': 'bmp'
-            }
+var Access = {};
+Access.auth = function (store, method, req, onSuccess, onError) {
+    console.log('我是自定义的权限验证函数');
+    onSuccess();
+};
+Access.store = {
+    picture: {
+        upload: true,
+        mimeType: {
+            'image/jpeg': 'jpg',
+            'image/png': 'png',
+            'image/gif': 'gif',
+            'image/bmp': 'bmp'
         }
     }
 };
